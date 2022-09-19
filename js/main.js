c752de041af06e7e0344c083fb2573e3b48b60ce
Lucia cataldo <lcataldo.lc@gmail.com>
2022-09-19 16:29:19 -0300

    primer entrega

--- js/main.js
+++ js/main.js
@@ -1,162 +1,151 @@
 
-//productos
-function Producto (producto){
-        this.id = producto.id;
-        this.nombre = producto.nombre;
-        this.marca = producto.marca;
-        this.precio = producto.precio;
-}
-
-const producto1 = new Producto({id:1, nombre:'parafina', marca:'summer', precio:500});
-const producto2 = new Producto({id:2, nombre:'quilla', marca:'freelife', precio:1000});
-const producto3 = new Producto({id:3, nombre:'pita', marca:'fcs', precio:800});
-const producto4 = new Producto({id:4, nombre:'deck', marca:'freelife', precio:1500});
-
-const listadoProductos = [producto1, producto2, producto3, producto4];
-
-//Saludo
-
-function saludar(){
-    alert ('Bienvenido a Point Norte Surf Shop!');
-    let nombreUsuario = prompt ('Por favor ingrese su nombre');
-    while (nombreUsuario === '') {
-        nombreUsuario = prompt ('Por favor ingrese su nombre');
+class Producto {
+    constructor (id, nombre, marca, categoria, precio){
+        this.id = id,
+        this.nombre = nombre,
+        this.marca = marca,
+        this.categoria = categoria,
+        this.precio = Number(precio);
     }
 }
-saludar();
 
-let queCompra = prompt ('Que producto quiere comprar? \nParafina \nQuilla \nPita \nDeck').toLocaleLowerCase();
-let queCantidad = parseInt(prompt('Que cantidad quiere comprar?'));
-
-function seleccionProducto() {
-    for (let i = 0; i < 5; i++){
-        if (queCompra == listadoProductos[i].nombre){
-        let precioFinal = queCantidad * listadoProductos[i].precio;
-
-        alert ('Su compra es ' + queCantidad + ' ' + queCompra + ' por un costo de $'+ precioFinal);
-        console.log(queCompra,queCantidad, precioFinal);
+const arrayProductos =[];
 
 
-            let comoAbona = prompt('Cómo quiere abonar? efectivo (descuento 20%) o cuotas (recargo 15%)').toLocaleLowerCase();
+const producto1 = new Producto (1, 'base', 'keepsurfing', 'parafina', 500);
+const producto2 = new Producto (2, 'summer', 'free life', 'parafina', 450);
+const producto3 = new Producto (3, 'winter','free life', 'parafina', 650);
 
-            if (comoAbona == "efectivo") {
-                let precioconDescuento = precioFinal - precioFinal *(20/100);
-                alert ('El precio final de su compra es $' + precioconDescuento  + " Gracias! vuelva pronto :)");
-                console.log(precioconDescuento);
-                } else if (comoAbona == "cuotas"){
-                    let precioconRecargo = precioFinal  + precioFinal  *(15/100);
-                    alert ("El precio final de su compra es $" + precioconRecargo+ " " + "Gracias! vuelva pronto :)");
-                    console.log(precioconRecargo);
-            } else{
-                alert ("No es una opcion válida :(");
+//Metodo Push
+arrayProductos.push(producto1, producto2, producto3);
 
-            }
+// Metodo Filter
+const marcaFreeLife = arrayProductos.filter ((free) => free.marca === 'free life');
+console.log (marcaFreeLife);
+
+
+// Metodo forEach
+const mostrarListaMenorMayor = () => {
+    const menorMayor = [];
+    arrayProductos.forEach (producto => menorMayor.push(producto.nombre + ' $'+producto.precio));
+    alert('Lista de Menor a Mayor precio ' + '\n'+ menorMayor.join('\n'));
+};
+
+const mostrarListaMayorMenor = () => {
+    const mayorMenor = [];
+    arrayProductos.forEach (producto => mayorMenor.push(producto.nombre + ' $'+producto.precio));
+    alert('Lista de Mayor a Menor precio ' + '\n'+ mayorMenor.join('\n'));
+};
+
+//Metodo Sort - menor a  mayor
+const ordenarPorPrecioMenor = () => {
+    arrayProductos.sort ((a,b)=> a.precio - b.precio);
+    console.log (arrayProductos);
+    mostrarListaMenorMayor();
+}
+
+
+//Metodo Sort - mayor a menor
+const ordenarPorPrecioMayor = () => {
+    arrayProductos.sort ((a,b)=> b.precio - a.precio);
+    mostrarListaMayorMenor ();
+}
+
+//Agregar Productos
+
+const agregarCarrito = () => {
+    let otroProducto;
+    let productoNombre = '';
+    let productoCantidad = 0;
+    let totalCompra =0;
+
+    do{
+        productoNombre = prompt('Qué queres comprar'+ '\n'+ producto1.nombre +'\n' + producto2.nombre + '\n'+ producto3.nombre).toLowerCase();
+        productoCantidad = parseInt(prompt('Cuántos querés comprar?'));
+
+        const productoSeleccionado = arrayProductos.find(producto => producto.nombre === productoNombre);
+        console.log(productoSeleccionado);
+
+
+        if(productoSeleccionado ){
+            totalCompra = productoSeleccionado.precio * productoCantidad;
+            alert('Agregó '+ productoNombre +',cantidad: '+ productoCantidad+ '\n El precio final es $'+ totalCompra);
+
+            console.log(productoSeleccionado.nombre, productoCantidad, totalCompra);
+
+        }else{
+            alert('El producto no se encuentra en el listado');
+        }
+
+        otroProducto = confirm('Desea seguir comprando?');
+
+
+    } while (otroProducto);
+
+    compraDescuento(totalCompra);
+};
+
+
+const compraDescuento = (totalCompra) => {
+    if (totalCompra >= 2500){
+        totalCompra = totalCompra * 0.85;
+        alert ('Tenés un descuento del 15%')
+    }
+    obtenerPrecioEnvio(totalCompra);
+}
+
+const obtenerPrecioEnvio = (totalCompra) => {
+    let confirmaCompra = confirm('Quiere que se lo enviemos a domicilio?');
+
+    if (confirmaCompra === totalCompra > 2500){
+        alert('Tenés envio gratis!');
+    } else if (confirmaCompra === totalCompra < 2500 && totalCompra !== 0){
+        totalCompra = totalCompra + 500;
+        alert('El costo de la compra y costo de envío es $'+totalCompra);
+    } else {
+        alert('El total de la compra es $'+totalCompra);
+    }
+    comoAbona(totalCompra);
+}
+
+
+
+
+const comoAbona = (totalCompra) => {
+    let comoPaga =  prompt('Quiere abonar en cuotas o efectivo?').toLowerCase();
+    console.log(comoPaga);
+
+    if (comoPaga === 'cuotas'){
+
+        let cuantasCuotas = parseInt (prompt('Quiere abonar en 3, 6 o 12 cuotas?')) ;
+
+        if (cuantasCuotas === 3 || cuantasCuotas === 6 || cuantasCuotas === 12){
+
+            let precioCuotas = totalCompra / cuantasCuotas;
+            alert ('Su compra de realizará en '+cuantasCuotas+' de $'+precioCuotas+'. Gracias vuelva pronto!' );
+
+        }else {
+            alert ('No es un numero de cuotas válidas' );
+            comoAbona();
 
         }
 
+    } else {
+        alert ('Su compra se realizará en efectivo. Gracias vuelva pronto!' );
     }
+}
+
+
+
+const ordenDeProductos = () =>{
+    if (confirm ('Quiere filtrar los productos de menor a mayor precio')){
+        ordenarPorPrecioMenor ();
+    } else {
+        ordenarPorPrecioMayor ();
+    }
+    agregarCarrito();
+
 
 }
 
-seleccionProducto();
-
-
-
-
-
-
-
-
-
-
-
-// //Saludo
-
-// function saludar(){
-//     alert ('Bienvenido a Point Norte Surf Shop!');
-//     let nombreUsuario = prompt ('Por favor ingrese su nombre');
-//     while (nombreUsuario === '') {
-//         nombreUsuario = prompt ('Por favor ingrese su nombre');
-
-//     }
-// }
-
-
-// //Seleccion producto
-// function seleccionProducto(){
-//     let idProducto;
-
-//     do{
-//         idProducto= prompt ('Qué producto quieres comprar? \n1.Quilla \n2.Parafina \n3.Pita') ;
-//     } while (idProducto !=1 &&  idProducto !=2 && idProducto !=3)
-
-//     switch (idProducto) {
-//         case '1':
-//             return  'QUILLA';
-//         case '2':
-//             return  'PARAFINA';
-//         case '3':
-//             return 'PITA'
-//     }
-// }
-
-// //Producto Nombre
-// function precioProducto(productoNombre){
-//     if (productoNombre === 'QUILLA'){
-//         return 500;
-//     }else if (productoNombre === 'PARAFINA'){
-//         return 800;
-//     }else if (productoNombre === 'PITA'){
-//         return 300;
-//     }
-// }
-
-// // Producto Cobrar
-
-// function cobrarProducto (productoNombre, precio ){
-//     let cantidadProducto = parseInt (prompt ('Que cantidad de '+productoNombre+ ' quiere comprar?'));
-//     let costoFinal = precio * cantidadProducto;
-//     console.log (costoFinal);
-
-//     alert ('El costo de su compra es $' +costoFinal );
-//     console.log(costoFinal);
-
-//     let comoAbona = prompt ('Quiere abonar en efectivo (20% de descuento) o en cuotas (15% de recargo)?').toLocaleLowerCase();
-//     console.log(comoAbona);
-
-//     if (comoAbona == "efectivo") {
-//         let precioconDescuento = precio - precio *(20/100);
-//         alert ("El precio final de su compra es $" + precioconDescuento + " " + "Gracias! vuelva pronto :)");
-//         console.log(precioconDescuento);
-//         } else {
-//             let precioconRecargo = precio + precio *(15/100);
-//         alert ("El precio final de su compra es $" + precioconRecargo+ " " + "Gracias! vuelva pronto :)");
-//         console.log(precioconRecargo);
-//     }
-
-// }
-
-
-// saludar();
-// let productoNombre = seleccionProducto();
-// console.log (productoNombre);
-// let precio =  precioProducto(productoNombre);
-// console.log (precio);
-// cobrarProducto(productoNombre, precio);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ordenDeProductos ();
